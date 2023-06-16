fdd6d2bbb947cbd5716eeca6ae76bbd7f9d3986f
Hamza Ezzaydia <hamzaydia@gmail.com>
2023-06-16 16:00:44 -0700

    fix 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,4 +2,5 @@
 def print_list_integer(my_list=None):
     if my_list is None:
         my_list = []
-    print('\n'.join(f'{i:d}' for i in my_list))
+    for i in my_list:
+        print('{:d}'.format(i))
