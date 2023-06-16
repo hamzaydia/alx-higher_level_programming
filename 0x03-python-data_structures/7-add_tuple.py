c6b77b5b4d25059d69730fd2785e0bd7ccb3a201
Hamza Ezzaydia <hamzaydia@gmail.com>
2023-06-16 16:45:25 -0700

    fix 7 edge case

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = tuple_a[:2] + (0, 0)[:2 - len(tuple_a)]
-    tuple_b = tuple_b[:2] + (0, 0)[:2 - len(tuple_b)]
+    tuple_a = tuple_a[:2]
+    tuple_b = tuple_b[:2]
 
     added_tuple = tuple(map(sum, zip(tuple_a, tuple_b)))
     return added_tuple
