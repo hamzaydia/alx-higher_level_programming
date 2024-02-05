af5b09722ddf54765c3249d055ffd672da794166
Hamza Ezzaydia <hamzaydia@gmail.com>
2024-02-05 15:45:18 -0800

    1. My list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module containing MyList class"""
+
+
+class MyList(list):
+    """class extends functionality from the `list` class."""
+
+    def print_sorted(self):
+        """Prints an int list in ascending order."""
+        print(sorted(self))
