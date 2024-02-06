de53788828853c06a3648c899384337aa9056b54
Hamza Ezzaydia <hamzaydia@gmail.com>
2024-02-05 18:33:35 -0800

    11. Square #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module of `Square` class """
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class """
+
+    def __init__(self, size):
+        """ Constructor method """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
