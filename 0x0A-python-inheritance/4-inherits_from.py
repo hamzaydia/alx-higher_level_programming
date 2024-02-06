ddd60f8ac6dccc87131a7c7b77a92441e47cb697
Hamza Ezzaydia <hamzaydia@gmail.com>
2024-02-05 18:06:26 -0800

    6. Improve Geometry

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,6 +3,5 @@
 
 
 def inherits_from(obj, a_class):
-    """ Checks if `obj` is an instance of a subclass of `a_class`.
-    Returns: True if `obj` is an instance of a subclass of `a_class`, False otherwise """
+    """ Checks if `obj` is an instance of a subclass of `a_class` """
     return (isinstance(obj, a_class) and (type(obj) is not a_class))
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Module for `BaseGeometry` class """
+
+
+class BaseGeometry:
+    """ a class """
+
+    def area(self):
+        """ Raise an Exception """
+        raise Exception("area() is not implemented")
