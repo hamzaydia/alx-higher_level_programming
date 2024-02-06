5cd6f971bb2fde516261b17990921f077a4653c3
Hamza Ezzaydia <hamzaydia@gmail.com>
2024-02-05 18:01:44 -0800

    5. Geometry module

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,7 +3,6 @@
 
 
 def inherits_from(obj, a_class):
-    """Checks if `obj` is an instance of a subclass of `a_class`.
-    Returns: True if `obj` is an instance of a subclass of `a_class`, False otherwise.
-    """
+    """ Checks if `obj` is an instance of a subclass of `a_class`.
+    Returns: True if `obj` is an instance of a subclass of `a_class`, False otherwise """
     return (isinstance(obj, a_class) and (type(obj) is not a_class))
