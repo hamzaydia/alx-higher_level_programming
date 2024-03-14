626d7f078b5263fd7fed8f9e6a51c243a90ca8f2
Hamza Ezzaydia <hamzaydia@gmail.com>
2024-03-14 09:52:58 -0700

    18. No Comedy tonight!

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,3 @@
+-- 18. No Comedy tonight!
+SELECT title FROM tv_shows WHERE title NOT IN ( SELECT s.title FROM tv_genres g, tv_show_genres t, tv_shows s WHERE g.id = t.genre_id AND t.show_id = s.id AND g.name = "Comedy" ) 
+ORDER BY title ASC;
